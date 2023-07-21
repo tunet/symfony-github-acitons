26ae52eb2cfb9a6fd46c77a20c1fb4a052b96543
aliaksandrkulba <tunet000@gmail.com>
2023-07-21 17:23:01 +0300

    debug

--- .github/actions/deploy/deploy.php
+++ .github/actions/deploy/deploy.php
@@ -4,3 +4,9 @@
 echo 'Test deploy PHP-script' . PHP_EOL;
 
 var_dump($_ENV);
+
+$files = new DirectoryIterator(__DIR__ . '/github/workspace');
+
+foreach ($files as $file) {
+    echo $file->getFilename() . PHP_EOL;
+}
