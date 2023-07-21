b8d8969ce87e9eb5c9365c0710b2985156a43a52
aliaksandrkulba <tunet000@gmail.com>
2023-07-21 16:22:54 +0300

    test

--- .github/actions/deploy/deploy.php
+++ .github/actions/deploy/deploy.php
@@ -4,7 +4,7 @@
 //$conn = ssh2_connect($_ENV['INPUT_SSH-HOST'], $_ENV['INPUT_SSH-PORT']);
 //ssh2_auth_password($conn, $_ENV['INPUT_SSH-USER'], $_ENV['INPUT_SSH-PASSWORD']);
 
-$files = new DirectoryIterator(__DIR__);
+$files = new DirectoryIterator(__DIR__ . '/github');
 
 foreach ($files as $file) {
     echo $file->getFilename() . PHP_EOL;
