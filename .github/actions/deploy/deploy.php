1b2c182a9bb36fba308a4fc471a676603a18b2b9
aliaksandrkulba <tunet000@gmail.com>
2023-07-21 16:18:01 +0300

    test

--- .github/actions/deploy/deploy.php
+++ .github/actions/deploy/deploy.php
@@ -1,6 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-echo 'Test PHP entrypoint' . PHP_EOL;
+$conn = ssh2_connect($_ENV['INPUT_SSH-HOST'], $_ENV['INPUT_SSH-PORT']);
+ssh2_auth_password($conn, $_ENV['INPUT_SSH-USER'], $_ENV['INPUT_SSH-PASSWORD']);
 
-var_dump($_ENV);
+echo 'Deploying...' . PHP_EOL;
